691d89ae9b1351c503b69f810aad7b1899a90eb1
Łukasz Mierzwa <l.mierzwa@gmail.com>
2009-09-23 21:53:54 +0200

    autodoc fixes

--- scripts/autodoc.sh
+++ scripts/autodoc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 make -C doc html
+git submodule foreach "git ls-files -o | xargs git add"
 git submodule foreach "git commit -a -m autocommit"
 git submodule foreach "git push origin master"
 git submodule sync
-
