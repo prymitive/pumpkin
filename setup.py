ae1caa923e7ba347abdf53d1ccadd115b1e23660
Łukasz Mierzwa <l.mierzwa@gmail.com>
2011-06-07 23:07:02 +0200

    0.1 development.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except ImportError:
 
 setup(
     name='pumpkin',
-    version='0.1.8',
+    version='0.1-dev',
     description='Simple library for working with ldap',
     author='Łukasz Mierzwa',
     author_email='l.mierzwa@gmail.com',
